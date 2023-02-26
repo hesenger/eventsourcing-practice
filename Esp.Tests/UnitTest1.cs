31811429417fa3e7865ad160117146d2a8702b87
Heber <sengerhe@gmail.com>
2023-02-26 10:10:07 -0300

    event stream red

--- Esp.Tests/UnitTest1.cs
+++ Esp.Tests/UnitTest1.cs
@@ -1,15 +1,46 @@
 namespace Esp.Tests;
 
-public class Tests
+public class AggregateRootTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void ApplyEventShouldStoreEventInStream()
     {
-        Assert.Pass();
+        var order = new Order();
+        var @event = new Event(1, "Order", 1, 1, "OrderCreated", "{}", DateTime.Now);
+
+        order.ApplyEvent(@event);
+
+        Assert.That(order.EventStream, Is.Not.Null);
+        Assert.That(order.EventStream, Has.Count.EqualTo(1));
+        Assert.That(order.EventStream[0], Is.EqualTo(@event));
     }
 }
+
+public class Order : IAggregateRoot
+{
+    public int Id { get; set; }
+    public List<Event> EventStream { get; set; }
+
+    public void ApplyEvent(Event @event)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IAggregateRoot
+{
+    int Id { get; }
+    List<Event> EventStream { get; }
+
+    void ApplyEvent(Event @event);
+}
+
+public record Event(
+    int Id,
+    string AggregateType,
+    int AggregateId,
+    int AggregateVersion,
+    string EventType,
+    string EventData,
+    DateTime EventDate
+);
