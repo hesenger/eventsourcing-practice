499555d044e36521a87f88ed3531b3315c51aff5
Heber <sengerhe@gmail.com>
2023-02-26 11:36:23 -0300

    placing order

--- Esp.Tests/AggregateRootTests.cs
+++ Esp.Tests/AggregateRootTests.cs
@@ -5,16 +5,39 @@ namespace Esp.Tests;
 public class AggregateRootTests
 {
     [Test]
-    public void InitializeShouldStoreEventInStream()
+    public void InitializeShouldRehydrateEventStream()
     {
         var order = new Order();
-        var @event = new Event(1, "Order", 1, 1, "Esp.Tests.OrderCreated", "{\"CreateAt\": \"2021-01-01 08:00\"}", DateTime.Now);
+        var @event = new Event(1, "Order", 1, 1, "Esp.Tests.OrderCreated", "{\"CreatedAt\": \"2021-01-01 08:00\"}", DateTime.Now);
 
         order.Initilize(1, new[] { @event });
 
-        Assert.That(order.GetEventStream(), Is.Not.Null);
-        Assert.That(order.GetEventStream(), Has.Count.EqualTo(1));
         Assert.That(order.GetEventStream()[0], Is.EqualTo(@event));
+    }
+
+    [Test]
+    public void InitializeShouldApplyEventToState()
+    {
+        var order = new Order();
+        var @event = new Event(1, "Order", 1, 1, "Esp.Tests.OrderCreated", "{\"CreatedAt\": \"2023-02-26T11:29:35.917815-03:00\"}", DateTime.Now);
+
+        order.Initilize(42, new[] { @event });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(order.Id, Is.EqualTo(42));
+            Assert.That(order.CreatedAt, Is.EqualTo(DateTime.Parse("2023-02-26T11:29:35.917815-03:00")));
+        });
+    }
+
+    [Test]
+    public void PlaceOrderShouldStoreEventInStream()
+    {
+        var order = new Order();
+
+        order.PlaceOrder(DateTime.Today);
+
+        Assert.That(order.CreatedAt, Is.EqualTo(DateTime.Today));
     }
 }
 
